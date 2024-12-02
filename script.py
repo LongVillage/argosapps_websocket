ed716f6d68030e4767c7de11d630fb029e610003
LongVillage <courtevillej@gmail.com>
2024-12-02 22:00:34 +0100

    Update script.py

--- script.py
+++ script.py
@@ -10,10 +10,8 @@ from datetime import datetime
 from pymysql.err import Error
 
 from cryptofeed import FeedHandler
-from cryptofeed.defines import CANDLES, OPEN_INTEREST, CandleInterval
-from cryptofeed.symbols import Symbol
-from cryptofeed.exchanges import Binance, BinanceFutures, BinanceDelivery
-from cryptofeed.callback import CandleCallback, OpenInterestCallback
+from cryptofeed.defines import CANDLES, OPEN_INTEREST
+from cryptofeed.exchanges import Binance, BinanceFutures
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -24,10 +22,10 @@ f = None
 feed_process = None
 
 def convert_to_sql_datetime(timestamp_ms):
-    newTimestramp = datetime.utcfromtimestamp(timestamp_ms).strftime('%Y-%m-%d %H:%M:%S')
-    return newTimestramp
+    newTimestamp = datetime.utcfromtimestamp(timestamp_ms).strftime('%Y-%m-%d %H:%M:%S')
+    return newTimestamp
 
-# Function to insert candle data into the database
+# Fonction pour insérer les données de bougie dans la base de données
 def insert_candle_into_db(candle_data, pair_id):
     host = "db-argos-sql.cfc44smyuydl.eu-north-1.rds.amazonaws.com"
     port = 3306
@@ -38,7 +36,7 @@ def insert_candle_into_db(candle_data, pair_id):
     try:
         conn = pymysql.connect(host=host, port=port, user=user, passwd=password, db=db)
         cursor = conn.cursor()
-        query = "INSERT INTO Candlestick (PairID, DateTime, Open, High, Low, Close, Volume, OpenInterest) VALUES (%s, %s, %s, %s, %s, %s,%s,%s)"
+        query = "INSERT INTO Candlestick (PairID, DateTime, Open, High, Low, Close, Volume, OpenInterest) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
         cursor.execute(query, (pair_id,) + candle_data)
         conn.commit()
         logging.info('Candle data inserted into DB')
@@ -49,27 +47,27 @@ def insert_candle_into_db(candle_data, pair_id):
         conn.close()
         print('Candle data inserted into DB')
 
-# Define a callback for candle
-async def candle_callback(feed, symbol, interval, receipt_timestamp, data):
+# Définition du callback pour les bougies
+async def candle_callback(feed, symbol, timestamp, receipt_timestamp, open_price, close_price, high_price, low_price, volume, closed, interval):
     pair_id = pair_id_mapping.get(symbol)
     if pair_id:
-        # Retrieve the latest open interest data
+        # Récupérer les dernières données d'Open Interest
         with open_interest_lock:
             open_interest = latest_open_interest.get(f"{symbol}-PERP", 0)
             print(f"{symbol}-PERP: {open_interest}")
 
         candle_data = (
-            convert_to_sql_datetime(data.timestamp),
-            float(data.open),
-            float(data.high),
-            float(data.low),
-            float(data.close),
-            float(data.volume),
+            convert_to_sql_datetime(timestamp),
+            float(open_price),
+            float(high_price),
+            float(low_price),
+            float(close_price),
+            float(volume),
             float(open_interest)
         )
         insert_candle_into_db(candle_data, pair_id)
 
-# Define a callback for open interest
+# Définition du callback pour l'Open Interest
 async def open_interest_handler(feed, symbol, open_interest, receipt_timestamp):
     with open_interest_lock:
         latest_open_interest[symbol] = open_interest
@@ -95,7 +93,7 @@ def parse_currency_pairs(json_data):
         pairs.append(symbol)
     return pairs
 
-# Main function
+# Fonction principale
 def run_feed_handler():
     global f
 
@@ -107,20 +105,15 @@ def run_feed_handler():
         if not argos_pairs:
             logging.error("Failed to fetch Argos pairs, stopping feed handler.")
             return
-        # currency_pairs = parse_currency_pairs(argos_pairs)
 
         currency_pairs = ["BTC-USDT", "ETH-USDT", "BNB-USDT", "SOL-USDT", "XRP-USDT"]
         currency_perp_pairs = [pair + '-PERP' for pair in currency_pairs]
 
         binancePairs = Binance.symbols()
-        # print(f"binancePairs: {binancePairs}")
         binanceFuturesPairs = BinanceFutures.symbols()
-        # print(f"binanceFuturesPairs {binanceFuturesPairs}")
 
         common_pairs = [pair for pair in currency_pairs if pair in binancePairs]
         common_perp_pairs = [pair for pair in currency_perp_pairs if pair in binanceFuturesPairs]
-        # print(common_pairs)
-        # print(common_perp_pairs)
 
         global pair_id_mapping
         pair_id_mapping = {
@@ -134,19 +127,19 @@ def run_feed_handler():
             f.stop()
 
         f = FeedHandler()
-        # BINANCE
+        # BINANCE FUTURES
         f.add_feed(BinanceFutures(
             symbols=common_perp_pairs,
             channels=[OPEN_INTEREST],
             callbacks={OPEN_INTEREST: open_interest_handler}
         ))
+        # BINANCE SPOT
         f.add_feed(Binance(
             symbols=common_pairs,
             channels=[CANDLES],
-            candle_interval=CandleInterval.INTERVAL_15M,
+            candle_interval='15m',  # Utilisation de l'intervalle en chaîne de caractères
             callbacks={CANDLES: candle_callback}
         ))
-        # f.add_feed(CoinBase(etc))
 
         f.run()
 
@@ -155,7 +148,7 @@ def run_feed_handler():
 
 def restart_feed_process():
     """
-    Safely terminates any existing feed handler process and starts a new one.
+    Termine en toute sécurité tout processus de gestionnaire de flux existant et en démarre un nouveau.
     """
     global feed_process
     if feed_process and feed_process.is_alive():
@@ -171,7 +164,7 @@ def restart_feed_process():
 
 def scheduled_run(interval=43200):
     """
-    Schedules the feed handler to restart at the specified interval (default is 12 hours).
+    Programme le gestionnaire de flux pour redémarrer à l'intervalle spécifié (par défaut toutes les 12 heures).
     """
     while True:
         restart_feed_process()
@@ -180,7 +173,7 @@ def scheduled_run(interval=43200):
 
 def start_scheduled_run():
     """
-    Starts the scheduled restart logic in a separate thread to avoid blocking the main application.
+    Démarre la logique de redémarrage programmé dans un thread séparé pour éviter de bloquer l'application principale.
     """
     from threading import Thread
 
@@ -205,22 +198,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# HNT-USDT: NOT IN
-# BSV-USDT: NOT IN
-# KAS-USDT: NOT IN
-# DOGE-USDC: NOT IN
-# 1000RATS-USDT: NOT IN
-# SRM-USDT: NOT IN
-# BTS-USDT: NOT IN
-# ORBS-USDT: NOT IN
-# 1000BONK-USDT: NOT IN
-# PYTH-USDT: NOT IN
-# 1000SHIB-USDT: NOT IN
-# TOKEN-USDT: NOT IN
-# BTCST-USDT: NOT IN
-# WIF-USDT: NOT IN
-# ONDO-USDT: NOT IN
-# BIGTIME-USDT: NOT IN
-# DEFI-USDT: NOT IN
-# ETHW-USDT: NOT IN
